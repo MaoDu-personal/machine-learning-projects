dbc36062a51cb4b28b96de5a4628752d08109e52
MaoDu <maodu.12@gmail.com>
2025-07-28 22:15:05 +0100

    test for sql script

---
+++ pnsd.sql
@@ -0,0 +1,7 @@
+% this code is to do the etl process for the pnsd dataset
+% the pnsd dataset is a dataset about the patient with the parkinson's disease
+% the dataset is from the UCI machine learning repository
+% the dataset is in the csv format
+% the dataset is about the patient with the parkinson's disease
+
+select * from pnsd
